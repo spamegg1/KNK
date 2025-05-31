4bd959be6752b38572acdc167feb92af9aaa9bee
spamegg <spamegg@pm.me>
2025-05-31 14:34:35 +0300

    ch12 programming projects 3,4,5

--- 12/pp1203.c
+++ 12/pp1203.c
@@ -1,11 +1,46 @@
 // Simplify Programming Project 1(b) by taking advantage of
 // the fact that an array name can be used as a pointer.
 
+// (a) Write a program that reads a message,
+// then prints the reversal of the message:
+// Enter a message: Don't get mad, get even.
+// Reversal is: .neve teg ,dam teg t'noD
+// Hint: Read the message one character at a time (using getchar)
+// and store the characters in an array.
+// Stop reading when the array is full or the character read is '\n'.
+// (b) Revise the program to use a pointer instead of
+// an integer to keep track of the current position in the array.
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAXLEN 100
+
+// (b)
+void print_reverse_ptr()
+{
+    char msg[MAXLEN];
+    char c;
+    char *p = msg;
+
+    printf("Enter a message: ");
+    while ((c = getchar()) != '\n' && p < msg + MAXLEN)
+    {
+        *p = c;
+        p++;
+    }
+
+    printf("Reversal is: ");
+    while (p >= msg)
+    {
+        putchar(*p);
+        p--;
+    }
+    printf("\n");
+}
+
 int main(void)
 {
-
+    print_reverse_ptr();
     return EXIT_SUCCESS;
 }
--- 12/pp1204.c
+++ 12/pp1204.c
@@ -1,11 +1,52 @@
 // Simplify Programming Project 2(b) by taking advantage
 // of the fact that an array name can be used as a pointer.
 
+// (a) Write a program that reads a message,
+// then checks whether it’s a palindrome
+// (the letters in the message are the same
+// from left to right as from right to left):
+// Enter a message: He lived as a devil, eh?
+// Palindrome
+// Enter a message: Madam, I am Adam.
+// Not a palindrome
+// Ignore all characters that aren’t letters.
+// Use integer variables to keep track of positions in the array.
+// (b) Revise the program to use pointers instead
+// of integers to keep track of positions in the array.
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h> /* isalpha, toupper */
+
+#define MAX_LEN 100
 
 int main(void)
 {
+    char message[MAX_LEN];
+    char ch;
+    char *p = message;
+    char *q;
 
+    printf("Enter a message: ");
+    while ((ch = toupper(getchar())) != '\n' && p < message + MAX_LEN)
+    {
+        if (isalpha(ch))
+        {
+            *p = ch;
+            p++;
+        }
+    }
+    p--;
+
+    for (q = message; q < p; q++, p--)
+    {
+        if (*p != *q)
+        {
+            printf("Not a palindrome\n");
+            return EXIT_SUCCESS;
+        }
+    }
+
+    printf("Is a palindrome\n");
     return EXIT_SUCCESS;
 }
