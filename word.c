e57ec8b056e9df34b8db9473b32f73b3d7d6e937
spamegg <spamegg@pm.me>
2025-06-21 14:31:03 +0300

    fix actions 4

--- word.c
+++ word.c
@@ -14,23 +14,29 @@
 
 int read_char(void)
 {
-  int ch = getchar();
+    int ch = getchar();
 
-  if (ch == '\n' || ch == '\t')
-    return ' ';
-  return ch;
+    if (ch == '\n' || ch == '\t')
+        return ' ';
+    return ch;
 }
 
 void read_word(char *word, int len)
 {
-  int ch, pos = 0;
+    int ch, pos = 0;
 
-  while ((ch = read_char()) == ' ')
-    ;
-  while (ch != ' ' && ch != EOF) {
-    if (pos < len)
-      word[pos++] = ch;
-    ch = read_char();
-  }
-  word[pos] = '\0';
+    while ((ch = read_char()) == ' ')
+        ;
+    while (ch != ' ' && ch != EOF)
+    {
+        if (pos < len)
+            word[pos++] = ch;
+        ch = read_char();
+    }
+    word[pos] = '\0';
+}
+
+int main(void)
+{
+    exit(EXIT_SUCCESS);
 }
