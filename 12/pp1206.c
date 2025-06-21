2d11690494b30b3777ce90b33a4a79a3983818f9
spamegg <spamegg@pm.me>
2025-06-21 10:33:13 +0300

    PP 12.06

--- 12/pp1206.c
+++ 12/pp1206.c
@@ -5,8 +5,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 10
+
+void quicksort(int a[], int *low, int *high);
+int *split(int a[], int *low, int *high);
+
 int main(void)
 {
+    int a[N], i;
 
-    return EXIT_SUCCESS;
+    printf("Enter %d numbers to be sorted: ", N);
+    for (i = 0; i < N; i++)
+        scanf("%d", &a[i]);
+
+    quicksort(a, a, a + N - 1); // sort in-place
+
+    printf("In sorted order: ");
+    for (i = 0; i < N; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+
+    exit(EXIT_SUCCESS);
+}
+
+void quicksort(int a[], int *low, int *high)
+{
+    int *middle;
+
+    if (low >= high)
+        return;
+    middle = split(a, low, high);
+    quicksort(a, low, middle - 1);
+    quicksort(a, middle + 1, high);
+}
+
+int *split(int a[], int *low, int *high)
+{
+    int part_element = *low;
+
+    for (;;)
+    {
+        while (low < high && part_element <= *high)
+            high--;
+        if (low >= high)
+            break;
+        *low++ = *high;
+
+        while (low < high && *low <= part_element)
+            low++;
+        if (low >= high)
+            break;
+        *high-- = *low;
+    }
+
+    *high = part_element;
+    return high;
 }
