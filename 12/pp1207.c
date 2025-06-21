80d5a5d32740ebe8a57cab48354400fb968f29c3
spamegg <spamegg@pm.me>
2025-06-21 14:19:32 +0300

    trying Github actions

--- 12/pp1207.c
+++ 12/pp1207.c
@@ -5,8 +5,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 10
+
+void max_min(int a[], int n, int *max, int *min);
+
 int main(void)
 {
+    int b[N], i, big, small;
 
-    return EXIT_SUCCESS;
+    printf("Enter %d numbers: ", N);
+    for (i = 0; i < N; i++)
+        scanf("%d", &b[i]);
+
+    max_min(b, N, &big, &small);
+
+    printf("Largest: %d\n", big);
+    printf("Smallest: %d\n", small);
+
+    exit(EXIT_SUCCESS);
+}
+
+void max_min(int a[], int n, int *max, int *min)
+{
+    int i;
+
+    *max = *min = a[0];
+    for (i = 1; i < n; i++)
+    {
+        if (a[i] > *max)
+            *max = a[i];
+        else if (a[i] < *min)
+            *min = a[i];
+    }
 }
